298daa2137f9eb62e4d871421c0c0bf232a56d28
Jeremy Grondin <jerem97480@gmail.com>
2020-12-20 19:58:28 +0100

    Fix UI button scale

--- Rubiks_cube/Assets/Scripts/GameManager.cs
+++ Rubiks_cube/Assets/Scripts/GameManager.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     RubiksCube cube;
+    [SerializeField]
+    GameObject cubeSizeText = null;
+
+    [SerializeField]
+    GameObject shuffleText = null;
+
     // Start is called before the first frame update
     void Start()
     {
         cube = GameObject.FindGameObjectWithTag("RubiksCube").GetComponent<RubiksCube>();
+        cubeSizeText.GetComponent<Text>().text = PlayerPrefs.GetInt("CubeSize").ToString();
+        shuffleText.GetComponent<Text>().text = PlayerPrefs.GetInt("Shuffle").ToString();
     }
 
     public void GoToMainMenu()
